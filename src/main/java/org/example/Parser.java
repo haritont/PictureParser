8e73a9cde2152d1f6d6bd271e2c3f9116464a095
Vika <haritont180402@gmail.com>
2022-11-11 20:47:00 +0300

    Добавила поля и конструктор

--- src/main/java/org/example/Parser.java
+++ src/main/java/org/example/Parser.java
@@ -1,13 +1,20 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Parser {
     private String url;
 
-    public Parser(){
+    private WebDriver driver;
 
+    public Parser(){
+        System.setProperty("webdriver.chrome.driver", "selenium\\chromedriver.exe");
+        driver = new ChromeDriver();
     }
+    
     public void setUrl(String url) {
         this.url = url;
     }
-    
+
 }
