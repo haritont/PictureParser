3a4ccbf148ee6c598b12cd118e4ca96908a31cd4
Vika <haritont180402@gmail.com>
2022-11-11 20:46:10 +0300

    Создала класс Parser

---
+++ src/main/java/org/example/Parser.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Parser {
+    private String url;
+
+    public Parser(){
+
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    
+}
