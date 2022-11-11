7025c4af4d9e60b6b8e05b1d49227a01981b5f4b
Vika <haritont180402@gmail.com>
2022-11-11 21:28:24 +0300

    Написала методы для скачивания картинки и для парсинга ее адреса

--- src/main/java/org/example/Parser.java
+++ src/main/java/org/example/Parser.java
@@ -6,6 +6,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class Parser {
@@ -32,10 +39,23 @@ public class Parser {
         Work();
     }
     private void downloadPicture(WebElement pictureWeb){
-        
+        try {
+            BufferedImage image =null;
+            URL connect = new URL(getUrlPicture(pictureWeb));
+            image = ImageIO.read(connect);
+            if (image != null){
+                ImageIO.write(image, "jpg",new File("picture\\"+pictureWeb.getText()+".jpg"));
+            }
+        }
+        catch (FileNotFoundException e) {
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-    private void getUrlPicture(WebElement pictureWeb){
-
+    private String getUrlPicture(WebElement pictureWeb){
+        return pictureWeb.getAttribute("src");
     }
     private void Work(){
         driver.findElement(new By.ByXPath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"))
@@ -45,9 +65,14 @@ public class Parser {
         driver.findElement(new By.ByXPath("//*[@id=\"hdtb-msb\"]/div[1]/div/div[2]/a"))
                 .click();
 
-        ArrayList<WebElement> pictures = (ArrayList<WebElement>) driver.findElement(new By.ByXPath("//*[@id=\"islrg\"]/div[1]/div"));
+        ArrayList<WebElement> pictures = (ArrayList<WebElement>) driver.findElements(new By.ByXPath("//*[@id=\"islrg\"]/div[1]"));
         for (int i=1;i<pictures.size();i++){
             WebElement pictureWeb = pictures.get(i-1);
+
+            JavascriptExecutor jsScroll = (JavascriptExecutor) driver;
+            jsScroll.executeScript("arguments[0].scrollIntoView();", pictureWeb);
+            pictures = (ArrayList<WebElement>) driver.findElements(new By.ByXPath("//*[@id=\"islrg\"]/div[1]"));
+
             downloadPicture(pictureWeb);
         }
     }
